cd90fbc286e6595e828a17ea77b376a9519d1246
ShaggyBa <semenvanchugov@mail.ru>
2023-03-26 21:32:55 +0300

    Исправил путь для запуска приложения

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from './Components/App';
 
 
-ReactDOM.render(<App/>, document.getElementById('root')) 
+ReactDOM.render(<App />, document.getElementById('root'))
 
 //!оставил на всякий дефолтную разметку
 // const root = ReactDOM.createRoot(document.getElementById('root'));
