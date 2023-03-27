ca002b398b9abe0d8e6c0cc6170759e912d238f4
oleksandr <oleksandrzelenjak@gmail.com>
2023-03-27 14:23:37 +0300

    added page folder

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import './input.css'
+
 
 const App = () => {
   return (
-    <h1 className="text-red-500">Hell</h1>
+    <h1 className="text-blue-500">Hell</h1>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './Components/App';
+import App from './components/app/App';
 
+import './style/input.css'
 
 ReactDOM.render(<App />, document.getElementById('root'))
 
