b51d3789cbef8864e1f1cba5a582483da4ec94bc
maevskyy <maevskijdimitrij1@gmail.com>
2023-03-27 11:54:10 +0300

    idk, just delete this commit

--- src/Components/App.js
+++ src/Components/App.js
@@ -9,31 +9,3 @@ const App = () => {
 
 export default App;
 
-
-// я
-//!оставил на всякий дефолтную разметку
-// import logo from './logo.svg';
-// import './App.css';
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-// export default App;
--- src/index.js
+++ src/index.js
@@ -3,12 +3,9 @@ import ReactDOM from 'react-dom';
 import App from './Components/App';
 
 
-ReactDOM.render(<App/>, document.getElementById('root')) 
-
-//!оставил на всякий дефолтную разметку
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// );
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
