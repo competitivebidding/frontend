4cd4c213b5f2ab5ff22d0bd310073a4facf2865c
maevskyy <maevskijdimitrij1@gmail.com>
2023-03-28 16:14:04 +0300

    added psges in firsteam branch

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -6,7 +6,7 @@ const App = () => {
     <div className="text-red-300">
       Dev branch fvf
       <div className="">
-        firstTeam
+        firstTeam f
       </div>
       <div className="">
 
