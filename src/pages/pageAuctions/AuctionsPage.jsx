7abefa273c778488da59be47ce7a86794c69f0ac
Klavdiy Tracevsky <klavdiy.tracevsky@gmail.com>
2023-03-31 02:52:18 +0300

    layout for all pages

--- src/pages/pageAuctions/AuctionsPage.jsx
+++ src/pages/pageAuctions/AuctionsPage.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
+import AppNavBar from "../../components/appNavBar/AppNavBar.jsx";
+import MainSection from "../../components/MainSection/MainSection.jsx";
 
 
 
 const AuctionsPage = ({title}) => {
   return (
-    <>
-    </>
+      <>
+        <AppNavBar/>
+        <MainSection PageContent={''} title={title}/> {/*Вставить свой контент*/}
+      </>
   );
 }
 
