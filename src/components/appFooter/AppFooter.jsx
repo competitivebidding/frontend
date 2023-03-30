0381c1d83e9853046604add6e35e3c49894c2d28
Oleksandr <oleksandrzelenjak@gmail.com>
2023-03-30 23:05:21 +0300

    added className

--- src/components/appFooter/AppFooter.jsx
+++ src/components/appFooter/AppFooter.jsx
@@ -5,16 +5,16 @@ import './AppFooter.scss';
 
 const AppFooter = () => {
   return (
-<footer class="footer">
+<footer className="footer">
     <nav> 
 
-        <ul class="footer__menu">
-            <li class="footer__menu-item"><a href="" class="footer__menu-link">Public offer</a></li>
-            <li class="footer__menu-item"><a href="" class="footer__menu-link">Terms of use</a></li>
-            <li class="footer__menu-item"><a href="" class="footer__menu-link">Security guarantee</a></li>
-            <li class="footer__menu-item"><a href="" class="footer__menu-link">Privacy Policy</a></li>
-            <li class="footer__menu-item"><a href="" class="footer__menu-link">Shipping and payment</a></li>
-            <li class="footer__menu-item"><a href="" class="footer__menu-link">Victory Tactics</a></li>
+        <ul className="footer__menu">
+            <li className="footer__menu-item"><a href="" className="footer__menu-link">Public offer</a></li>
+            <li className="footer__menu-item"><a href="" className="footer__menu-link">Terms of use</a></li>
+            <li className="footer__menu-item"><a href="" className="footer__menu-link">Security guarantee</a></li>
+            <li className="footer__menu-item"><a href="" className="footer__menu-link">Privacy Policy</a></li>
+            <li className="footer__menu-item"><a href="" className="footer__menu-link">Shipping and payment</a></li>
+            <li className="footer__menu-item"><a href="" className="footer__menu-link">Victory Tactics</a></li>
         </ul>
     </nav>
     <p class="footer__copyright">COMPETITIVE BIDDING© 2023. All rights reserved</p>
