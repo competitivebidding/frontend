3d394053522be92303947d309f7a47ffb21a33a7
Daniil <danechka.bo@yandex.ru>
2023-03-31 19:07:11 +0300

    NewsPage with two img in news function

--- src/components/pages/pageNews/NewsList-item/NewsList-item.jsx
+++ src/components/pages/pageNews/NewsList-item/NewsList-item.jsx
@@ -1,13 +1,27 @@
 import React from 'react';
 import './NewsList-item.scss';
 
-const NewsListItem = ({img, alt, title, date, text}) => {
+const NewsListItem = ({img1, img2, alt, title, date, text}) => {
+    const imgBox = ()=> {
+        if(img2){
+            return(
+                <div className="listItem__imgbox">
+                    <img src={img1} alt={alt} />
+                    <img src={img2} alt={alt} />
+                </div>
+        )
+        } else {
+            return(
+                <div className="listItem__imgbox">
+                    <img src={img1} alt={alt} />
+                </div>
+            )
+        }
+    }
     
     return (
         <div className='listItem'>
-            <div className="listItem__imgbox">
-                <img src={img} alt={alt} />
-            </div>
+            {imgBox()}
             <div className="lsitItem__header">
                 <div className="listItem__title">{title}</div>
                 <div className="listItem__date">{date}</div>
