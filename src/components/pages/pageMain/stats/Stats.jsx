035e2c99dbac27cf99e3cb38ebb30829fbd8e65f
ShaggyBa <71100001+ShaggyBa@users.noreply.github.com>
2023-04-04 20:36:34 +0300

    1.02

--- src/components/pages/pageMain/stats/Stats.jsx
+++ src/components/pages/pageMain/stats/Stats.jsx
@@ -5,7 +5,7 @@ import "./Stats.scss"
 const Stats = (props) => {
 	return (
 		<div className="stats__item">
-			<div className="stats_title">{props.title}</div>
+			<div className="stats__title">{props.title}</div>
 			<div className="stats__content">{props.text}</div>
 			<div className="stats__image">
 				<img src={props.image || ""} alt="" />
