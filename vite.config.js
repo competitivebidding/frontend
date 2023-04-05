53e7fc8bbfb4c7a04ff1980854042add297aef01
maevskyy <maevskijdimitrij1@gmail.com>
2023-04-05 12:00:40 +0300

    added a little userInfo

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+const path = require('path')
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@assets': path.resolve(__dirname, './src/assets')
+    }
+  }
 })
