fed2a2c9b7d34bdea99dd89c60e90f8672fe7189
Yernar <ernar_9.6@bk.ru>
2023-04-07 19:32:39 +0600

    firstTeam-Notifications

---
+++ src/pages/pageNotifications/NotificationCard.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const NotificationCard = ({id,title,text,link, date,time,prizeImg}) => {
+  return (
+              <div className='note'>
+                <img className='note__note-img' src="src/pages/pageNotifications/img/prize.svg" alt="prize" />
+                <div className='note-information' >
+                  <div className='note-infomation__note-texts'>
+                    <h1 className='note-information__note-texts__note-title'> {title}</h1>
+                    <p className='note-information__note-texts__note-text'>{text}</p>
+                    <a href='#' className='note-information__note-texts__note-link'>{link} </a>
+                  </div>
+                  <div className='note-information__note-dates'>
+                    <h2 className='note-information__note-dates__note-date'>{date} </h2>
+                    <h3 className='note-information__note-dates__note-time'>{time}
+                    </h3>
+                  </div>
+                </div>
+              </div>
+  )
+}
+
+export default NotificationCard
