ee07ad3827828b58a71e5a4b92cfd22af988088c
Klavdiy Tracevsky <klavdiy.tracevsky@gmail.com>
2023-04-19 14:01:15 +0300

    header with filters done

--- src/pages/pagePartnerProgram/Statistic/Lists/PartnersLists.jsx
+++ src/pages/pagePartnerProgram/Statistic/Lists/PartnersLists.jsx
@@ -1,35 +1,37 @@
 import React, {useState} from 'react';
 import styles from "./PartnersLists.module.scss"
+import PartnersListFilter from "@/pages/pagePartnerProgram/Statistic/Lists/PartnersListFilter";
 
 
-function PartnersLists(props) {
+function PartnersLists() {
+    const [isClicked, setIsClicked] = useState(false);
 
     return (
         <div className={styles.lists}>
             <div className={styles.header}>
                 <div className={styles.header__filter + ' ' + styles.header__filter_partner}>
                     <h4>Partner</h4>
-                    <span className={styles.icon}></span>
+                    <PartnersListFilter isClicked={isClicked}/>
                 </div>
                 <div className={styles.header__filter + ' ' + styles.header__filter_join}>
                     <h4>Join date</h4>
-                    <span className={styles.icon}></span>
+                    <PartnersListFilter isClicked={isClicked}/>
                 </div>
                 <div className={styles.header__filter + ' ' + styles.header__filter_spentM}>
                     <h4>Spent this month</h4>
-                    <span className={styles.icon}></span>
+                    <PartnersListFilter isClicked={isClicked}/>
                 </div>
                 <div className={styles.header__filter + ' ' + styles.header__filter_earnedM}>
                     <h4>Earned this month</h4>
-                    <span className={styles.icon}></span>
+                    <PartnersListFilter isClicked={isClicked}/>
                 </div>
                 <div className={styles.header__filter + ' ' + styles.header__filter_spent}>
                     <h4>Total earns</h4>
-                    <span className={styles.icon}></span>
+                    <PartnersListFilter isClicked={isClicked}/>
                 </div>
                 <div className={styles.header__filter + ' ' + styles.header__filter_earned}>
                     <h4>Total spends</h4>
-                    <span className={styles.icon}></span>
+                    <PartnersListFilter isClicked={isClicked}/>
                 </div>
             </div>
         </div>
