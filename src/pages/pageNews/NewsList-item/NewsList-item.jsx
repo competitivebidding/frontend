f58425db90278ed09ea8b81c52d1382efa9502a8
ShaggyBa <71100001+ShaggyBa@users.noreply.github.com>
2023-04-27 14:36:31 +0300

    GraphQL запрос для страницы news

--- src/pages/pageNews/NewsList-item/NewsList-item.jsx
+++ src/pages/pageNews/NewsList-item/NewsList-item.jsx
@@ -1,35 +1,35 @@
 import React from 'react'
 import './NewsList-item.scss'
 
-const NewsListItem = ({ img1, img2, alt, title, date, text }) => {
+const NewsListItem = ({ images, title, date, text }) => {
+	const newDate = new Date(date)
+	const ImgItem = () => {
+		if (images && typeof images === 'object') {
+			const getImages = images.map(image => <img src={image || "/src/pages/pageNews/img/quare.png"} alt={""} />)
+			return (
+				<div className="listItem__imgbox">
+					{getImages}
+				</div>
+			)
+		} else {
+			return (
+				<div className="listItem__imgbox">
+					<img src={images || "/src/pages/pageNews/img/quare.png"} alt={""} />
+				</div>
+			)
+		}
+	}
 
-  const ImgItem = () => {
-    if (img2) {
-      return (
-        <div className="listItem__imgbox">
-          <img src={img1} alt={alt} />
-          <img src={img2} alt={alt} />
-        </div>
-      )
-    } else {
-      return (
-        <div className="listItem__imgbox">
-          <img src={img1} alt={alt} />
-        </div>
-      )
-    }
-  }
-
-  return (
-    <div className="listItem">
-      <ImgItem />
-      <div className="lsitItem__header">
-        <div className="listItem__title">{title}</div>
-        <div className="listItem__date">{date}</div>
-      </div>
-      <div className="listItem__text">{text}</div>
-    </div>
-  )
+	return (
+		<div className="listItem">
+			<ImgItem />
+			<div className="lsitItem__header">
+				<div className="listItem__title">{title}</div>
+				<div className="listItem__date">{newDate.toUTCString()}</div>
+			</div>
+			<div className="listItem__text">{text}</div>
+		</div>
+	)
 }
 
 export default NewsListItem
