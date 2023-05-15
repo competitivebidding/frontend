27db51fd72559e51752b25360cd4e7de86ad0ecc
Yernar <ernar_9.6@bk.ru>
2023-05-16 00:12:28 +0600

    TokensY

--- src/pages/pageTokens/TokenCard.jsx
+++ src/pages/pageTokens/TokenCard.jsx
@@ -1,15 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
 import TokensPage from './TokensPage'
 
+
+
 const TokenCard = ({tokens,prize}) => {
-   
+  const [modal,setModal]=useState(false)
+  
+  const toggleButton=()=>{
+    setModal(!modal)
+  }
+ 
+    
   return (
-        <div className='token'>
-          <h1 className='token__header'>{tokens}</h1>
-          <h2 className='token__prize'>{prize}</h2>
-          <button className='token__button'> Купить </button>
+    <> 
+    <div className='token' >
+      <h1 className='token__header'>{tokens}</h1>
+      <h2 className='token__prize'>{prize}</h2>
+      <button className='token__button' onClick={toggleButton} > Buy </button>
+    </div>
+      {modal&&
+        <div className='modal' onClick={toggleButton} >
+          
+          <div className='modal__content' onClick={e=>e.stopPropagation()} >         
+            <h3 className='modal__header' >Congratulations!</h3>
+       
+            <h4 className='modal__tokens' >+ {tokens}</h4>
+            <p className='modal__text'>Your account has been replenished <br/> by {tokens}</p>
+            <button className='modal__button' onClick={toggleButton}>Excellent</button>
         </div>
+          
+       
+    </div>}
+        
+
+  </>
   )
-}
+  }
 
 export default TokenCard
--- src/pages/pageTokens/TokensPage.jsx
+++ src/pages/pageTokens/TokensPage.jsx
@@ -1,29 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
 import AppHeader from '../../components/appHeader/AppHeader.jsx'
 import './TokensPage.scss'
 import TokenCard from './TokenCard.jsx'
 
+
+
 const TokensPage = ({ title }) => {
    const data=[{
     tokens:'1 000 ROTO',
-    prize:'1 000 ₽'
-    },{
+    prize:'12 $',
+    id:1},
+      {
       tokens:'2 500 ROTO',
-      prize:'1 999 ₽'
+      prize:'25 $',
+      id:2
       },
       {
         tokens:'7 500 ROTO',
-        prize:'5 499 ₽'
+        prize:'70 $',
+        id:3
         },{
           tokens:'15 000 ROTO',
-          prize:'9 999 ₽'
+          prize:'125 $',
+          id:4
           }]
+
   return (
     <>
       <AppHeader title={title} />
       <div className='tokens'>
-        {data.map((obj)=>(<TokenCard tokens={obj.tokens} prize={obj.prize}/>))  }
+        {data.map((obj,index)=>(<TokenCard tokens={obj.tokens} prize={obj.prize} key={index} />))  }
       </div>
+     
     </>
   )
 }
