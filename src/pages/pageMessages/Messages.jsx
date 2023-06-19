36674b4175ef22f6efb1c087e9d4900850506271
Oleksandr <oleksandrzelenjak@gmail.com>
2023-06-19 21:59:06 +0300

    added new modal in chat

--- src/pages/pageMessages/Messages.jsx
+++ src/pages/pageMessages/Messages.jsx
@@ -6,19 +6,25 @@ import iconPlus from '../../assets/Chat/iconPlus.svg';
 import { AppModal } from '../../components/appModal/AppModal';
 
 const Messages = () => {
-  const [modalGroup, setModal] = useState(false);
+  const [modalNewGroup, setModalNewGroup] = useState(false);
+  const [modalGroup, setModalGroup] = useState(false);
+
   const [groupName, setGroupName] = useState('');
-  const [groups, setGroups] = useState([]);
+  const [Newgroups, setNewGroups] = useState([]);
   const [rotationAngle, setRotationAngle] = useState(0);
 
-  const toggleGroup = () => {
-    setModal(!modalGroup);
+  const toggleNewGroup = () => {
+    setModalNewGroup(!modalNewGroup);
     setRotationAngle(angle => angle + 180);
   };
 
-  const addGroup = () => {
-    setGroups(g => [...g, groupName]);
-    toggleGroup();
+  const toggleGroup = () => {
+    setModalGroup(!modalGroup);
+  };
+
+  const addNewGroup = () => {
+    setNewGroups(g => [...g, groupName]);
+    toggleNewGroup();
     setGroupName('');
   };
 
@@ -28,7 +34,7 @@ const Messages = () => {
         <div className="chat__sidebar sidebar">
           <h2>Group</h2>
           <ul className="sidebar__group">
-            {groups.map(group => (
+            {Newgroups.map(group => (
               <li className="sidebar__list">{group}</li>
             ))}
           </ul>
@@ -36,16 +42,17 @@ const Messages = () => {
             <img
               src={iconPlus}
               alt="iconGroup"
-              onClick={toggleGroup}
+              onClick={toggleNewGroup}
               style={{ transform: `rotate(${rotationAngle}deg)` }}
-              className={modalGroup ? 'group__rotate' : ''}
+              className={modalNewGroup ? 'group__rotate' : ''}
             />
           </div>
         </div>
 
         <div className="chat__container">
-          <div className="chat__header">
+          <div className="chat__header" onClick={toggleGroup}>
             <h2>Group 1</h2>
+            <div className='chat__subscribers'>222 subscribers</div>
           </div>
 
           <div className="chat__messages message">
@@ -70,22 +77,52 @@ const Messages = () => {
         </div>
       </div>
 
-      {modalGroup && (
-        <AppModal isOpen={modalGroup} onClose={toggleGroup}>
-          <div className="modalGroup">
-            <div className="modalGroup__title">Enter your group name</div>
+      {modalNewGroup && (
+        <AppModal isOpen={modalNewGroup} onClose={toggleNewGroup}>
+          <div className="modalNewGroup">
+            <div className="modalNewGroup__title">Enter your group name</div>
             <input
               type="text"
-              className="modalGroup__name"
+              className="modalNewGroup__name"
               value={groupName}
               onChange={e => setGroupName(e.target.value)}
             />
-            <button className="modalGroup__button" onClick={addGroup}>
+            <button className="modalNewGroup__button" onClick={addNewGroup}>
               Create Group
             </button>
           </div>
         </AppModal>
       )}
+
+{modalGroup && (
+  <AppModal isOpen={modalGroup} onClose={toggleGroup}>
+    <div className="modalGroup">
+      <div className="modalGroup__header">
+        <div className="modalGroup__avatar">
+          <img src="group_avatar.png" alt="Group Avatar" />
+        </div>
+        <div className="modalGroup__info">
+          <h2>Name chat</h2>
+          <p>description chat</p>
+        </div>
+      </div>
+      <div className="modalGroup__members">
+        <h3>33 members</h3>
+        <ul>
+          <li>Oleg Ivanov</li>
+          <li>Oleg Ivanov</li>
+          <li>Oleg Ivanov</li>
+          <li>Oleg Ivanov</li>
+          <li>Oleg Ivanov</li>
+        </ul>
+      </div>
+      <div className="modalGroup__auctions">
+        <button className="leaveButton">Leave chat</button>
+      </div>
+    </div>
+  </AppModal>
+)}
+
     </>
   );
 };
