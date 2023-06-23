6c8fa791f555a8fa7fc18a6fcabad4cb6b962fac
ShaggyBa <semenvanchugov@mail.ru>
2023-06-23 18:45:15 +0300

    1.00

--- src/pages/pagePartnerProgram/UserInfo/UserInfo.jsx
+++ src/pages/pagePartnerProgram/UserInfo/UserInfo.jsx
@@ -6,14 +6,39 @@ import UserInfoButtons from './UserInfoComponents/UserInfoButtons';
 import UserInfoAccount from './UserInfoComponents/UserInfoAccount';
 import ForPartners from "@/pages/pagePartnerProgram/UserInfo/UserInfoComponents/ForPartners";
 
+import { useQuery } from '@apollo/client'
+import { GET_PROFILE_QUERY } from '../../../components/server/userProfile'
+
+
 const UserInfo = () => {
-    return (
-        <section className='block'>
-            <UserInfoButtons/>
-            <UserInfoAccount/>
-            <ForPartners/>
-        </section>
-    );
+
+	const [userData, setUserData] = React.useState(null)
+	const [isLoaded, setIsLoaded] = React.useState(false)
+
+	const { data, error, loading, refetch } = useQuery(GET_PROFILE_QUERY)
+
+	React.useEffect(() => {
+		if (!loading && data?.getProfile) {
+			setUserData(data.getProfile)
+			setIsLoaded(!isLoaded)
+		}
+	}, [data])
+
+
+	return (
+		<>
+			{
+				!isLoaded
+					?
+					<h2>Loading...</h2>
+					:
+					<section className='block'>
+						<UserInfoButtons />
+						<UserInfoAccount id={userData?.id} />
+						<ForPartners />
+					</section>}
+		</>
+	);
 }
 
 export default UserInfo;
