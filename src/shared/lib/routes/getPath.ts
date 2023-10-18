b6901fe7511ded1b0943ea8d01f0762233247910
d9m0n4 <c2h8e9d4@gmail.com>
2023-10-18 21:59:00 +0300

    header titles

--- src/shared/lib/routes/getPath.ts
+++ src/shared/lib/routes/getPath.ts
@@ -1,4 +1,4 @@
-import { routesConfig } from '@shared/lib/routes/routesConfig'
+import {routesConfig} from "@shared/lib/routes/routesConfig";
 
 export const getPageTitle = (location: string) => {
   const currentRoute = Object.values(routesConfig).find(val => {
